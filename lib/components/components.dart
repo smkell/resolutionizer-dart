9ad06ebbf899012754b53db137bf1e7e2366a1c0
Sean Kell <sean.michael.kell@gmail.com>
2015-01-05 10:23:58 -0500

    Add rating component.
    Rating component is used to display stars for editing and viewing the rating of the recipe.
    
    Fixes #1

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -3,3 +3,4 @@ library resolutionizer.components;
 import 'package:angular/angular.dart';
 
 part 'recipe_book.dart';
+part 'rating.dart';
--- test/resolutionizer_test.dart
+++ test/resolutionizer_test.dart
@@ -23,4 +23,28 @@ main() {
       expect(recipeBook.selectedRecipe, same(recipe));
     }));
   });
+
+  group('rating component', () {
+    setUp(module((Module m) => m.install(new ResolutionizerApp())));
+
+    test('should show the right number of stars', inject((RatingComponent rating) {
+      rating.maxRating = '5';
+      expect(rating.stars, equals([1,2,3,4,5]));
+    }));
+
+    test('should handle click', inject((RatingComponent rating) {
+      rating.maxRating = '5';
+      rating.handleClick(3);
+      expect(rating.rating, equals(3));
+
+      rating.handleClick(1);
+      expect(rating.rating, equals(1));
+
+      rating.handleClick(1);
+      expect(rating.rating, equals(0));
+
+      rating.handleClick(1);
+      expect(rating.rating, equals(1));
+    }));
+  });
 }
